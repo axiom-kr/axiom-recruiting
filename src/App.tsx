6714e2d40c332dfd67b329a656f06c16c701a8dd
i4song <i4song.dev@gmail.com>
2024-01-27 17:08:18 +0900

    :building_construction: Create basic architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <div>Hello World!</div>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Program from "./pages/Program";
+import JoinUs from "./pages/JoinUs";
+import Contact from "./pages/Contact";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/recruit" element={<JoinUs />} />
+        <Route path="/program" element={<Program />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
