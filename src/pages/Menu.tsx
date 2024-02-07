e4ff4fe4b3b2df0c07ad2a6b35ab4dd594c02824
rroyc20 <rroyc20@gmail.com>
2024-02-08 07:44:58 +0900

    Create Menu Page Prototype

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,13 +1,37 @@
 import { Link } from "react-router-dom";
 
 const Menu = () => {
-  return <div>
-    <Link to="/">HOME</Link><br/>
-    <Link to="/program">PROGRAM</Link><br/>
-    <Link to="/about">ABOUT</Link><br/>
-    <Link to="/recruit">JOIN US</Link><br/>
-    <Link to="/contact">CONTACT</Link>
-  </div>;
+  return (
+    <div id="menu">
+      <div className="menu__container">
+        <ul className="menu__list">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/program">Program</Link>
+          </li>
+          <li>
+            <Link to="/recruit">Join us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="logo__container">
+        <div id="menu__title">
+          <h1 className="title__h1">
+            A<em>X</em>IOM
+          </h1>
+          <span className="title__span">settle for beyond</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
